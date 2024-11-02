27009aa9e7e8fa37d8bcf9223289c22a6a770fd2
Micamaster100 <98234233+Micamaster100@users.noreply.github.com>
2024-11-02 15:26:25 -0400

    Update CharacterConfiguration.lua

--- Scripts/CharacterConfiguration.lua
+++ Scripts/CharacterConfiguration.lua
@@ -1,9 +1,3 @@
---[[
-Title: Character Configuration
-Author: @Micamaster100
-Description: Framework for basic character configuration.
-]]
-
 local Players = game.Players
 local Player = Players.LocalPlayer
 local Character = Player.Character
