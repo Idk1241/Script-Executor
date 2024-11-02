bb48cfa22fd413c26c1a0e74b2f0bee187f408f0
Micamaster100 <98234233+Micamaster100@users.noreply.github.com>
2024-11-02 12:10:50 -0400

    Update CharacterConfiguration.lua

--- Scripts/CharacterConfiguration.lua
+++ Scripts/CharacterConfiguration.lua
@@ -1,1 +1,11 @@
--- test
+--[[
+Title: Character Configuration
+Author: @Micamaster100
+Description: Framework for basic character configuration.
+]]
+
+local Players = game.Players
+local Player = Players.LocalPlayer
+local Character = Player.Character
+local Humanoid: Humanoid = Character.Humanoid
+local HumanoidRootPart: BasePart = Character.HumanoidRootPart
