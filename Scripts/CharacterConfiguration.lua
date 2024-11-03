0fcef8bc775f39a09a84219b37a1ca20438d3c7e
Micamaster100 <98234233+Micamaster100@users.noreply.github.com>
2024-11-02 20:33:09 -0400

    Update CharacterConfiguration.lua

--- Scripts/CharacterConfiguration.lua
+++ Scripts/CharacterConfiguration.lua
@@ -1,5 +1,5 @@
 local Players = game.Players
 local Player = Players.LocalPlayer
-local Character = Player.Character
+local Character = Player.Character or Player.CharacterAdded:Wait()
 local Humanoid: Humanoid = Character.Humanoid
 local HumanoidRootPart: BasePart = Character.HumanoidRootPart
